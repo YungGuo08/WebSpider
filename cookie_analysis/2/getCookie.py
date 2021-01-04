acfb3520323cf4df3c081be369765d0c3721b623
YungGuo08 <65215647+YungGuo08@users.noreply.github.com>
2021-01-04 17:00:11 +0800

    Update getCookie.py

--- cookie_analysis/2/getCookie.py
+++ cookie_analysis/2/getCookie.py
@@ -7,21 +7,26 @@ from requests.utils import add_dict_to_cookiejar
 
 
 def getCookie(data):
+    """
+    通过加密对比得到正确cookie参数
+    :param data: 参数
+    :return: 返回正确cookie参数
+    """
     chars = len(data['chars'])
     for i in range(chars):
         for j in range(chars):
-            encrypt_chars = data['bts'][0] + data['chars'][i] + data['chars'][j] + data['bts'][1]
-            ha = None
+            clearance = data['bts'][0] + data['chars'][i] + data['chars'][j] + data['bts'][1]
+            encrypt = None
             if data['ha'] == 'md5':
-                ha = hashlib.md5()
+                encrypt = hashlib.md5()
             elif data['ha'] == 'sha1':
-                ha = hashlib.sha1()
+                encrypt = hashlib.sha1()
             elif data['ha'] == 'sha256':
-                ha = hashlib.sha256()
-            ha.update(encrypt_chars.encode())
-            result = ha.hexdigest()
+                encrypt = hashlib.sha256()
+            encrypt.update(clearance.encode())
+            result = encrypt.hexdigest()
             if result == data['ct']:
-                return encrypt_chars
+                return clearance
 
 
 url = 'https://www.cnvd.org.cn/flaw/list.htm'
@@ -29,13 +34,16 @@ header = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
                   'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36'
 }
+# 使用session保持会话
 session = requests.session()
 res1 = session.get(url, headers=header)
 jsl_clearance_s = re.findall(r'cookie=(.*?);location', res1.text)[0]
+# 执行js代码
 jsl_clearance_s = str(execjs.eval(jsl_clearance_s)).split('=')[1].split(';')[0]
 # add_dict_to_cookiejar方法添加cookie
 add_dict_to_cookiejar(session.cookies, {'__jsl_clearance_s': jsl_clearance_s})
 res2 = session.get(url, headers=header)
+# 提取go方法中的参数
 data = json.loads(re.findall(r';go\((.*?)\)', res2.text)[0])
 jsl_clearance_s = getCookie(data)
 # 修改cookie
